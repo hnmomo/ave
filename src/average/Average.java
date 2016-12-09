10fa4663ab180b0f7b4802c32a1207559cbb5c72
kent <772039670@qq.com>
2016-12-09 12:51:51 -0500

    Update Average.java

--- src/average/Average.java
+++ src/average/Average.java
@@ -19,17 +19,27 @@ public class Average {
      */
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
+        //l is how many decks there are
         int l=in.nextInt();
+        //integers in a is the number of cards in each deck corresponding to the index
         int[]a=new int[l];
+        //ave is the average number of all cards, ans is the final answer
         int ave=0,ans=0;
+        //read data
         for(int i=0;i<l;i++){
             a[i]=in.nextInt();
+            //add number to average
             ave+=a[i];
         }
+        //calculate average
         ave/=l;
+        //for each deck except for the last one
         for(int i=0;i<l-1;i++){
+            //if it needs cards to become equal to average
             if(a[i]!=ave){
+                //move cards
                 a[i+1]-=a[i]-ave;
+                //add 1 to the answer
                 ans++;
             }
         }
